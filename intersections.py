3c52c9ae8994fe079f9ca836e3a5701a6905763a
Tate Mioton <tatemioton@outlook.com>
2020-03-26 00:14:06 -0500

    Add 'scale', 'draw' Methods to Rectangle; Change 'intersects' Parameter Name

--- intersections.py
+++ intersections.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+from pyglet.graphics import draw_indexed
+from pyglet.gl import GL_LINES
+
 
 @dataclass
 class Rectangle:
@@ -8,10 +11,31 @@ class Rectangle:
     w: float
     h: float
 
-    def intersects(self, rect: "Rectangle"):
+    def intersects(self, other: "Rectangle"):
         return not (
-            rect.x > self.x + self.w
-            or rect.x + rect.w < self.x
-            or rect.y > self.y + self.h
-            or rect.y + rect.h < self.y
+            other.x > self.x + self.w
+            or other.x + other.w < self.x
+            or other.y > self.y + self.h
+            or other.y + other.h < self.y
         )
+
+    def scale(self, sx: float, sy: float):
+        """
+        zyyyz ) where:
+        xooox )   o = Rectangle(..., ..., o, o) (original)
+        xooox )   x = Rectangle(..., ..., x, 0) (scale x)
+        xooox )   y = Rectangle(..., ..., 0, y) (scale y)
+        zyyyz )   z = Rectangle(..., ..., x, y) (corners)
+        """
+        return Rectangle(
+            self.x - sx,     self.y - sy,
+            self.w + sx * 2, self.h + sy * 2
+        )
+
+    def draw(self):
+        draw_indexed(4, GL_LINES, [0, 1, 1, 2, 2, 3, 3, 0], ('v2f', [
+            self.x, self.y,
+            self.x + self.w, self.y,
+            self.x + self.w, self.y + self.h,
+            self.x, self.y + self.h
+        ]))
