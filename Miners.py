d20ddfbd79f36f7f5358583defbc8978aa4318e5
Tate Mioton <tatemioton@outlook.com>
2020-03-26 22:16:05 -0500

    Move Shape Class and Standard Shapes to 'shapes' File

--- Miners.py
+++ Miners.py
@@ -12,76 +12,14 @@ I think we do a check over all loaded blocks to see
 if they fall within the viewport. If not, we do VertexList.delete,
 else, we add to the batch.
 """
-from dataclasses import dataclass
-from math import cos, sin
-from typing import List, Tuple, Optional, Union
+from typing import List, Tuple, Optional
 
-import numpy as np
 import pyglet
 
 from pyglet.graphics.vertexdomain import VertexList
 
 from intersections import Rectangle
-
-
-@dataclass
-class Shape:
-    mesh: np.ndarray
-    mode: int
-    indices: List[int]
-
-    def transform(
-            self, dx: float, dy: float, angle:
-            float, sx: float, sy: float
-    ) -> np.ndarray:
-        return self.mesh @ np.array([
-            [sx * cos(angle),  sx * sin(angle), 0., 0.],
-            [sy * -sin(angle), sy * cos(angle), 0., 0.],
-            [0.,               0.,              1., 0.],
-            [dx,               dy,              0., 1.]
-        ], dtype=float)
-
-    def transform_no_rotate(self, dx: float, dy: float, sx: float, sy: float):
-        return self.mesh @ np.array([
-            [sx, 0., 0., 0.],
-            [0., sy, 0., 0.],
-            [0., 0., 1., 0.],
-            [dx, dy, 0., 1.]
-        ], dtype=float)
-
-    def add_to_batch(
-            self, batch: pyglet.graphics.Batch,
-            group: pyglet.graphics.Group = None,
-            *data: Union[str, Tuple[str, Union[Tuple[float, ...], List[float]]]]
-    ) -> pyglet.graphics.vertexdomain.VertexList:
-        return batch.add_indexed(
-            4, self.mode, group,
-            self.indices, *data
-        )
-
-
-quad = Shape(
-    np.array([
-        [0., 0., 0., 1.],
-        [1., 0., 0., 1.],
-        [1., 1., 0., 1.],
-        [0., 1., 0., 1.]
-    ], dtype=float),
-    pyglet.gl.GL_TRIANGLES,
-    [0, 1, 2, 2, 3, 0]
-)
-
-
-line_quad = Shape(
-    np.array([
-        [0., 0., 0., 1.],
-        [1., 0., 0., 1.],
-        [1., 1., 0., 1.],
-        [0., 1., 0., 1.]
-    ], dtype=float),
-    pyglet.gl.GL_LINES,
-    [0, 1, 1, 2, 2, 3, 3, 0]
-)
+from shapes import line_quad
 
 
 class Block:
---
+++ shapes.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from typing import List, Union, Tuple
+
+import numpy as np
+import pyglet
+from math import cos, sin
+
+
+@dataclass
+class Shape:
+    mesh: np.ndarray
+    mode: int
+    indices: List[int]
+
+    def transform(
+            self, dx: float, dy: float, angle:
+            float, sx: float, sy: float
+    ) -> np.ndarray:
+        return self.mesh @ np.array([
+            [sx * cos(angle),  sx * sin(angle), 0., 0.],
+            [sy * -sin(angle), sy * cos(angle), 0., 0.],
+            [0.,               0.,              1., 0.],
+            [dx,               dy,              0., 1.]
+        ], dtype=float)
+
+    def transform_no_rotate(self, dx: float, dy: float, sx: float, sy: float):
+        return self.mesh @ np.array([
+            [sx, 0., 0., 0.],
+            [0., sy, 0., 0.],
+            [0., 0., 1., 0.],
+            [dx, dy, 0., 1.]
+        ], dtype=float)
+
+    def add_to_batch(
+            self, batch: pyglet.graphics.Batch,
+            group: pyglet.graphics.Group = None,
+            *data: Union[str, Tuple[str, Union[Tuple[float, ...], List[float]]]]
+    ) -> pyglet.graphics.vertexdomain.VertexList:
+        return batch.add_indexed(
+            4, self.mode, group,
+            self.indices, *data
+        )
+
+
+quad = Shape(
+    np.array([
+        [0., 0., 0., 1.],
+        [1., 0., 0., 1.],
+        [1., 1., 0., 1.],
+        [0., 1., 0., 1.]
+    ], dtype=float),
+    pyglet.gl.GL_TRIANGLES,
+    [0, 1, 2, 2, 3, 0]
+)
+
+line_quad = Shape(
+    np.array([
+        [0., 0., 0., 1.],
+        [1., 0., 0., 1.],
+        [1., 1., 0., 1.],
+        [0., 1., 0., 1.]
+    ], dtype=float),
+    pyglet.gl.GL_LINES,
+    [0, 1, 1, 2, 2, 3, 3, 0]
+)
