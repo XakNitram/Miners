f019597cd3176d75cb25cc1825f005d5d1873b49
Tate Mioton <tatemioton@outlook.com>
2020-03-30 18:23:10 -0500

    Use Camera from pyglet Examples
    The from-scrap camera class was close enough to the pyglet example camera to warrant just including that one.

--- Miners.py
+++ Miners.py
@@ -23,35 +23,73 @@ from shapes import line_quad
 
 
 class Camera:
-    __slots__ = ['x', 'y', "width", "height"]
+    """
+    A simple 2D camera that contains the speed and offset.
+    Extended from the pyglet example camera
+    """
 
-    near = -1.
-    far = 1.
+    def __init__(self, scroll_speed=1, min_zoom=1, max_zoom=4):
+        assert min_zoom <= max_zoom, "Minimum zoom must not be greater than maximum zoom"
+        self.scroll_speed = scroll_speed
+        self.max_zoom = max_zoom
+        self.min_zoom = min_zoom
+        self.offset_x = 0
+        self.offset_y = 0
+        self._zoom = max(min(1, self.max_zoom), self.min_zoom)
 
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+    @property
+    def zoom(self):
+        return self._zoom
 
-    def move(self, dx, dy):
-        self.x -= 10. * dx
-        self.y -= 10. * dy
+    @zoom.setter
+    def zoom(self, value):
+        """ Here we set zoom, clamp time_data to minimum of min_zoom and max of max_zoom."""
+        self._zoom = max(min(value, self.max_zoom), self.min_zoom)
 
-    def project(self):
-        pyglet.gl.glMatrixMode(pyglet.gl.GL_PROJECTION)
-        pyglet.gl.glLoadIdentity()
-        pyglet.gl.glOrtho(
-            self.x, self.x + self.width,
-            self.y, self.y + self.height,
-            self.near, self.far
-        )
+    @property
+    def position(self):
+        """Query the current offset."""
+        return self.offset_x, self.offset_y
 
-        pyglet.gl.glMatrixMode(pyglet.gl.GL_MODELVIEW)
+    @position.setter
+    def position(self, value):
+        """Set the scroll offset directly."""
+        self.offset_x, self.offset_y = value
+
+    def move(self, axis_x, axis_y):
+        """ Move axis direction with scroll_speed.
+            Example: Move left -> move(-1, 0)
+         """
+        self.offset_x += self.scroll_speed * axis_x
+        self.offset_y += self.scroll_speed * axis_y
+
+    def begin(self):
+        # Set the current camera offset so you can draw your scene.
+        # Translate using the zoom and the offset.
+        pyglet.gl.glTranslatef(-self.offset_x * self._zoom, -self.offset_y * self._zoom, 0)
+
+        # Scale by zoom level.
+        pyglet.gl.glScalef(self._zoom, self._zoom, 1)
+
+    def end(self):
+        # Since this is a matrix, you will need to reverse the translate after rendering otherwise
+        # it will multiply the current offset every draw update pushing it further and further away.
+
+        # Reverse scale, since that was the last transform.
+        pyglet.gl.glScalef(1 / self._zoom, 1 / self._zoom, 1)
+
+        # Reverse translate.
+        pyglet.gl.glTranslatef(self.offset_x * self._zoom, self.offset_y * self._zoom, 0)
+
+    def __enter__(self):
+        self.begin()
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.end()
 
     @property
     def rectangle(self) -> Rectangle:
-        return Rectangle(self.x, self.y, self.width, self.height)
+        return Rectangle(self.offset_x, self.offset_y, 800, 640)
 
 
 class Block:
@@ -167,32 +205,25 @@ class Simulation:
         self.window.push_handlers(self.on_draw)
 
         self.batch = pyglet.graphics.Batch()
-        self.camera = Camera(0., 0., width, height)
+        self.camera = Camera(10)
+        self.gui_camera = Camera(10)
 
         self.board = Board(self.batch, self.camera, self.width, self.height)
 
     def update(self, dt: float):
-        dx = self.keys[pyglet.window.key.A] - self.keys[pyglet.window.key.D]
-        dy = self.keys[pyglet.window.key.S] - self.keys[pyglet.window.key.W]
+        dx = self.keys[pyglet.window.key.D] - self.keys[pyglet.window.key.A]
+        dy = self.keys[pyglet.window.key.W] - self.keys[pyglet.window.key.S]
         self.camera.move(dx, dy)
 
         self.board.update(dt)
 
     def on_draw(self):
         self.window.clear()
-        self.batch.draw()
+        with self.camera:
+            self.batch.draw()
 
-        pyglet.graphics.draw(
-            4, pyglet.gl.GL_QUADS,
-            ('v2f/static', [
-                self.width - 75, self.height - 75,
-                self.width - 25, self.height - 75,
-                self.width - 25, self.height - 25,
-                self.width - 75, self.height - 25
-            ])
-        )
-
-        self.camera.project()
+        with self.gui_camera:
+            self.gui_camera.rectangle.scale(-200., -200.).draw()
 
     def setup(self):
         pyglet.clock.schedule_interval(
