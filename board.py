b26ac620d2cb9cc31af45ef87df60fc1ca44a04d
Tate Mioton <tatemioton@outlook.com>
2020-05-23 15:38:14 -0500

    Remove Miners Temporarily; Implement Priority Subchunk Loading
    Much work still has to be done before the miners are properly included. They miners here were added a bit too early.
    
    The chunk now loads visual subchunks based on what the player can see.

--- board.py
+++ board.py
@@ -1,132 +1,32 @@
 from collections import deque
 from dataclasses import dataclass
-from math import atan2, tau
-from random import random, shuffle, randrange
+from random import random, randrange
 from typing import Optional, Tuple, List, Deque, Set, Union
 
-from pyglet import gl
-from pyglet.graphics import Group, OrderedGroup, Batch
+from math import atan2, tau
+from pyglet.graphics import OrderedGroup, Batch
 from pyglet.graphics.vertexdomain import VertexList
-from pyglet.image import Texture
-from pyglet.resource import texture
-from pyglet.sprite import Sprite
 
 from camera import Camera
 from common import global_timer, GRID_SIZE
 from intersections import Rectangle
-from shapes import quad, line_quad
+from shapes import line_quad, quad
 
 
-class TextureGroup(Group):
-
-    def __init__(
-            self, tex: Texture,
-            parent: Group = None
-    ):
-        super().__init__(parent)
-        self.texture = tex
-        self.blend_src = gl.GL_SRC_ALPHA
-        self.blend_dest = gl.GL_ONE_MINUS_SRC_ALPHA
-
-    def set_state(self):
-        gl.glEnable(self.texture.target)
-        gl.glBindTexture(self.texture.target, self.texture.id)
-
-        gl.glTexParameteri(
-            gl.GL_TEXTURE_2D,
-            gl.GL_TEXTURE_MAG_FILTER,
-            gl.GL_NEAREST
-        )
-
-        gl.glPushAttrib(gl.GL_COLOR_BUFFER_BIT)
-        gl.glEnable(gl.GL_BLEND)
-        gl.glBlendFunc(self.blend_src, self.blend_dest)
-
-    def unset_state(self):
-        gl.glPopAttrib()
-        gl.glDisable(self.texture.target)
-
-
-@dataclass
-class Position:
-    x: float = 0.
-    y: float = 0.
-
-
-@dataclass
-class Index:
-    i: int = 0
-    j: int = 0
+BLOCKS = OrderedGroup(0)
+MINERS = OrderedGroup(1)
+BORDER = OrderedGroup(2)
 
 
 @dataclass
 class Block:
-    # position: Position = Position(0., 0.)  # why did we want this?
     broken: bool = False
     value: int = 0
 
 
-# TEXTURES = [
-#         texture("Data/Environment/FLOOR_01.png"),
-#         texture("Data/Environment/FLOOR_02.png"),
-#         texture("Data/Environment/FLOOR_03.png")
-#     ]
-#
-# MINER_TEXTURE = texture("Data/Environment/pickaxe.png")
-# MINER_TEXTURE.width *= 2
-# MINER_TEXTURE.height *= 2
-#
-BLOCKS = OrderedGroup(0)
-MINERS = OrderedGroup(1)
-BORDER = OrderedGroup(2)
-# GROUPS = [TextureGroup(TEXTURES[i], BLOCKS) for i in range(3)]
-
-
-class Miner:
-    SPEED = 1.
-
-    def __init__(self, x, y, batch: Batch):
-        self.position = Position(x, y)
-        self.index = Index()
-
-        # self.sprite = Sprite(
-        #     MINER_TEXTURE, self.position.x, self.position.y,
-        #     batch=batch, group=MINERS, subpixel=True
-        # )
-
-        self.vbo = batch.add_indexed(
-            4, quad.mode, MINERS, quad.indices,
-            ('v4f', quad.transform_no_rotate(x, y, GRID_SIZE, GRID_SIZE).flatten())
-        )
-
-        self.move_time = 0.
-
-    def teleport(self):
-        size = GRID_SIZE
-
-        i = randrange(1, -2, -1)
-        j = randrange(1, -2, -1)
-        self.index.i += i
-        self.index.j += j
-        x = self.position.x + i * size
-        y = self.position.y + j * size
-
-        # self.sprite.update(x, y)
-        self.vbo.vertices = quad.transform_no_rotate(x, y, size, size).flatten()
-        self.position.x = x
-        self.position.y = y
-
-    def update(self, dt: float):
-        self.move_time += dt
-
-        if self.move_time >= self.SPEED:
-            self.move_time = 0.
-            self.teleport()
-
-
 class Chunk:
-    # BLOCK_SHAPE = quad
-    BLOCK_SHAPE = line_quad
+    BLOCK_SHAPE = quad
+    # BLOCK_SHAPE = line_quad
 
     def __init__(self, name: int, offset: Tuple[float, float]):
         self.blocks: List[Block] = [Block(value=randrange(3)) for _ in range(256)]
@@ -144,7 +44,6 @@ class Chunk:
         return f"Chunk({self.name:d}, ({x:f}, {y:f}))"
 
     def hide(self):
-        """Disable the chunk. Will be later converted to asynchronous code."""
         # print(f"disabling the chunk {self.name}")
 
         if not self.visible:
@@ -161,7 +60,6 @@ class Chunk:
                 self.bound = None
 
     def show(self, batch: Batch, view_box: Rectangle):
-        """Start the chunk loading process."""
         # print(f"enabling the chunk {self.name}")
 
         if self.visible:
@@ -169,39 +67,63 @@ class Chunk:
             self.show_queue.clear()
 
             # angle-based loading
-            # vx, vy = view_box.center
-            # sx, sy = self.rectangle.center
-            #
-            # # load in 4 x 4 squares
-            # angle = atan2(vy - sy, vx - sx) % tau
-            # t16 = tau / 16
-            # if t16 * 15 < angle + tau < tau + t16:
-            #     for i in range(4):
-            #         for j in range(4):
-            #             self.show_queue.append((i, j))
-            # elif t16 < angle < tau * 3:
-            #     for n in range(1, -1, -1):
-            #         for m in range(1, -1, -1):
-            #             for j in range(1, -1, -1):
-            #                 for i in range(1, -1, -1):
-            #                     self.show_queue.append((m * 2 + i, n * 2 + j))
-            # elif t16 * 7 < angle < t16 * 9:
-            #     for i in range(3, -1, -1):
-            #         for j in range(4):
-            #             self.show_queue.append((i, j))
-            # elif t16 * 3 < angle < t16 * 5:
-            #     for j in range(4):
-            #         for i in range(3, -1, -1):
-            #             self.show_queue.append((i, j))
-            # elif t16 * 11 < angle < t16 * 13:
-            #     for j in range(3, -1, -1):
-            #         for i in range(4):
-            #             self.show_queue.append((i, j))
+            bound_box = self.rectangle
+            vbcx, vbcy = view_box.center
+            bbcx, bbcy = bound_box.center
 
-            for j in range(4):
+            eighth = tau / 8
+            sixteenth = tau / 16
+
+            # % tau converts negative angles to standard angles
+            angle = (atan2(vbcy - bbcy, vbcx - bbcx) % tau)
+
+            # this only works for a camera zoom showing a 3x3 grid.
+            # load_order: List[Tuple[int, int]] = [None] * 16
+            if 7 * eighth - sixteenth < angle or angle <= sixteenth:
+                for i in range(3, -1, -1):
+                    for j in range(4):
+                        self.show_queue.append((i, j))
+            elif sixteenth < angle <= 2 * eighth - sixteenth:
+                for p in range(1, -1, -1):
+                    for q in range(1, -1, -1):
+                        for i in range(1, -1, -1):
+                            for j in range(1, -1, -1):
+                                self.show_queue.append((p * 2 + i, q * 2 + j))
+            elif 2 * eighth - sixteenth < angle <= 3 * eighth - sixteenth:
+                for j in range(3, -1, -1):
+                    for i in range(4):
+                        self.show_queue.append((i, j))
+            elif 3 * eighth - sixteenth < angle <= 4 * eighth - sixteenth:
+                for p in range(2):
+                    for q in range(1, -1, -1):
+                        for i in range(2):
+                            for j in range(1, -1, -1):
+                                self.show_queue.append((p * 2 + i, q * 2 + j))
+            elif 4 * eighth - sixteenth < angle <= 5 * eighth - sixteenth:
                 for i in range(4):
-                    self.show_queue.append((i, j))
-            shuffle(self.show_queue)
+                    for j in range(4):
+                        self.show_queue.append((i, j))
+            elif 5 * eighth - sixteenth < angle <= 6 * eighth - sixteenth:
+                for p in range(2):
+                    for q in range(2):
+                        for i in range(2):
+                            for j in range(2):
+                                self.show_queue.append((p * 2 + i, q * 2 + j))
+            elif 6 * eighth - sixteenth < angle <= 7 * eighth - sixteenth:
+                for j in range(4):
+                    for i in range(4):
+                        self.show_queue.append((i, j))
+            else:
+                for p in range(1, -1, -1):
+                    for q in range(2):
+                        for i in range(1, -1, -1):
+                            for j in range(2):
+                                self.show_queue.append((p * 2 + i, q * 2 + j))
+
+            # for j in range(4):
+            #     for i in range(4):
+            #         self.show_queue.append((i, j))
+            # shuffle(self.show_queue)
 
             self.bound = line_quad.add_to_batch(
                 batch, BORDER, (
@@ -211,14 +133,14 @@ class Chunk:
                 )
             )
 
-    def process(self, batch) -> bool:
+    def process(self, batch: Batch) -> bool:
         if not len(self.show_queue):
             return True
 
         scale = GRID_SIZE
         ox, oy = self.offset
         for _ in range(4):
-            i, j = self.show_queue.pop()
+            i, j = self.show_queue.popleft()
             for n in range(4):
                 for m in range(4):
                     index = (j * 64) + (i * 16) + (n * 4) + m
@@ -248,6 +170,11 @@ class Chunk:
 class ChunkGrid:
     """Construct to handle chunk loading and unloading."""
 
+    @dataclass
+    class Loadable:
+        chunk: int
+        finished: bool = False
+
     def __init__(self, initial_width, initial_height):
         self.chunks: List[Chunk] = []
 
@@ -261,7 +188,7 @@ class ChunkGrid:
         #     Chunk(1, (offset_x1, offset_y1)),
         #     Chunk(2, (offset_x2, offset_y2)),
         #         ...,
-        #     CHunk(n, (w2 + cx * (n - xr / 2, h2 + cy * (n - xr / 2))
+        #     Chunk(n, (w2 + cx * (n - xr / 2, h2 + cy * (n - xr / 2))
         # ]
 
         for j in range(yr):
@@ -277,7 +204,7 @@ class ChunkGrid:
             set(),  # load vbos
             set(),  # delete vbos
             set(),  # load data
-            set()   # save data
+            set()   # cache data
         ]
 
         self.current: int = -1
@@ -332,20 +259,11 @@ class Board:
         self.camera = camera
 
         self.chunks: ChunkGrid = ChunkGrid(init_width, init_height)
-        ox, oy = self.chunks[12].offset
-        i, j = randrange(16), randrange(16)
-        x = (i * GRID_SIZE) + ox
-        y = (j * GRID_SIZE) + oy
-        self.miners = []
-        for i in range(10):
-            self.miners.append(Miner(x, y, batch))
 
     @global_timer.timed
     def update(self, dt):
         # the scale added will depend on the camera's movement speed.
         # view_box = self.camera.rectangle.scale(-200., -200.)
+        # view_box = self.camera.rectangle.scale(200., 200.)
         view_box = self.camera.rectangle
-
-        for miner in self.miners:
-            miner.update(dt)
         self.chunks.process_graphics(self.batch, view_box)
